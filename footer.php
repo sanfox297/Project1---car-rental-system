ee96594a25fdf1b49c95e6b41f4c5a0032b653e5
Pratik Pande <103176587+sanfox297@users.noreply.github.com>
2022-10-26 13:36:38 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p></p>
+					<ul>
+						<li><a href="http://facebook.com" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved  CONTACT US ON : 7219350235/7264823735</a>
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
